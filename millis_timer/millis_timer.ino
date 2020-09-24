f228cd7d3ec2f1e82f61ad0212119dbb140a2c1a
forexms78 <49295076+forexms78@users.noreply.github.com>
2020-09-24 14:02:44 +0900

    Add files via upload

---
+++ millis_timer/millis_timer.ino
@@ -0,0 +1,23 @@
+unsigned long time;
+int count = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  time = millis();
+
+  if((time/100) % 1 == 0)
+  {
+  Serial.print("0.1초마다 반응 : ");
+  Serial.println(count++);
+  }
+    if((time/100) % 20 == 0)
+  {
+    Serial.println("2초마다 반응 : ");
+  }
+  
+  //Serial.println(time/100);
+  delay(100);
+}
